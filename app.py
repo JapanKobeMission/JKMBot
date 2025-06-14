e5ec74bca496df4f27ff39b4d4355956cec44e59
JKM Tech <japankobetech@gmail.com>
2025-06-14 17:11:11 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from googletrans import Translator
+import os
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+translator = Translator()
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+    lang = 'ja' if text[0].isascii() else 'en'
+    translated = translator.translate(text, dest=lang)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=translated.text)
+    )
+
+if __name__ == "__main__":
+    app.run()
