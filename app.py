c10beacf47416fddb73734d87868b5fc649b7c81
JKM Tech <japankobetech@gmail.com>
2025-06-14 17:46:48 +0900

    Changed to group chat enabled bot (maybe, using GPT lol)

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from googletrans import Translator
+
 import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+# LINE credentials from environment variables
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 translator = Translator()
 
-@app.route("/callback", methods=['POST'])
+@app.route("/")
+def health_check():
+    return "Bot is running!"
+
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
 
     try:
@@ -21,17 +31,20 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    lang = 'ja' if text[0].isascii() else 'en'
-    translated = translator.translate(text, dest=lang)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=translated.text)
-    )
 
-if __name__ == "__main__":
-    app.run()
+    # Detect language
+    lang = 'ja' if all(ord(c) < 128 for c in text) else 'en'
+
+    try:
+        translated = translator.translate(text, dest=lang).text
+    except Exception as e:
+        translated = "Translation error: " + str(e)
+
+    reply = TextSendMessage(text=translated)
+    line_bot_api.reply_message(event.reply_token, reply)
+
